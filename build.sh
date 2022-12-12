8d590f9c14cce80183f757e5f7a4fe58450c718b
Edo Barnas <iam@edobarnas.com>
2022-12-12 11:04:20 +0100

    Add `PHP 8.2.0`

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ do
 done
 
 PS3='Choose a version: '
-OPTIONS=("7.3" "7.4" "8.0")
+OPTIONS=("7.3" "7.4" "8.0" "8.1" "8.2")
 
 echo "Which PHP version do you want to use?"
 select OPT in "${OPTIONS[@]}";
